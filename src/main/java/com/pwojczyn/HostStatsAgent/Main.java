9d368dd101bf3f34a36c56f13dcab8867a3a7ef4
pwojczyn <pwojczyn@gmail.com>
2017-11-05 15:16:13 +0100

    v1.0

--- src/main/java/com/pwojczyn/HostStatsAgent/Main.java
+++ src/main/java/com/pwojczyn/HostStatsAgent/Main.java
@@ -4,6 +4,7 @@ import com.pwojczyn.HostStatsAgent.javasysmon.JavaSysMon;
 
 public class Main {
     public static void main(String[] args) {
+        // cmd: java -jar hoststatsagent.jar bfc6c0979f536fe36745f96f5818d268 &
         String apikey;
         System.out.println("HostStatsAgent 1.0");
         if (args.length == 0){
